df47651b835a4f5fb8b244940af03ba6300143c4
cozy <cozychen@liontravel.com>
2023-10-11 15:43:32 +0800

    add tailwind plugin/base

--- apps/test/tailwind.config.ts
+++ apps/test/tailwind.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import twPlugin from "ui/plugin";
+import twPlugin from "ui/plugin/base";
 // import { mergedStyles } from "ui/plugin";
 
 export default {
