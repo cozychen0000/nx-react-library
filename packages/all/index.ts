e31990d5fb52aaadfa9e20ef697b7599c1596961
cozy <cozychen@liontravel.com>
2023-10-16 10:07:09 +0800

    change packages peerDependencies

--- packages/all/index.ts
+++ packages/all/index.ts
@@ -2,3 +2,4 @@ export * as Hooks from "nx-react-library-hooks"
 export * as Ui from "nx-react-library-ui"
 export * as Icons from "nx-react-library-icons"
 export * as Utils from "nx-react-library-utils"
+
--- packages/ui/components/button/button.tsx
+++ packages/ui/components/button/button.tsx
@@ -14,28 +14,28 @@ interface ButtonBaseProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 
+const baseStyles =
+  'rounded font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2';
+
+const sizeStyles = {
+  sm: 'py-1 px-2 text-sm',
+  md: 'py-2 px-4',
+  lg: 'py-3 px-6 text-lg',
+};
+
+const colorStyles = {
+  primary: {
+    filled: 'bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-400',
+    outlined:
+      'bg-transparent hover:bg-blue-100 text-blue-500 border-blue-500 border focus:ring-blue-400',
+  },
+  secondary: {
+    filled: 'bg-gray-500 hover:bg-gray-600 text-white focus:ring-gray-400',
+    outlined:
+      'bg-transparent hover:bg-gray-100 text-gray-500 border-gray-500 border focus:ring-gray-400',
+  },
+};
 export function Button(props: ButtonBaseProps) {
-  const baseStyles =
-    'rounded font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2';
-
-  const sizeStyles = {
-    sm: 'py-1 px-2 text-sm',
-    md: 'py-2 px-4',
-    lg: 'py-3 px-6 text-lg',
-  };
-
-  const colorStyles = {
-    primary: {
-      filled: 'bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-400',
-      outlined:
-        'bg-transparent hover:bg-blue-100 text-blue-500 border-blue-500 border focus:ring-blue-400',
-    },
-    secondary: {
-      filled: 'bg-gray-500 hover:bg-gray-600 text-white focus:ring-gray-400',
-      outlined:
-        'bg-transparent hover:bg-gray-100 text-gray-500 border-gray-500 border focus:ring-gray-400',
-    },
-  };
 
   const buttonStyles = `${baseStyles} ${sizeStyles[props.size]} ${colorStyles[props.color][props.variant]}`;
 
@@ -43,3 +43,9 @@ export function Button(props: ButtonBaseProps) {
   return <button className={`btn ${buttonStyles}`} onClick={() => props.onClick()}>{props.children}</button>;
 }
 export default Button;
+
+Button.defaultProps = {
+  variant: "filled",
+  size: "md",
+  color: "primary"
+};
